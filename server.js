70bfbe3f76a5fa28119feb093097aabea0ef1378
James Chen <james.lai.chen@gmail.com>
2016-05-01 16:05:58 -0700

    minor changes

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.static(__dirname + "/views"));
 
 // START SERVER
 // ===========================================================================
-app.listen(app.get("port") || 7000);
+app.listen(process.env.PORT || 7000);
 console.log("Express server listening on port " + app.get("port"));
 
 // ROUTES FOR OUR API
