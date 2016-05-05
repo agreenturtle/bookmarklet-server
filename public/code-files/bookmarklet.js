c157e5a6805cae8f406a43dfeabbef78fd4163cd
James Chen <james.lai.chen@gmail.com>
2016-05-05 10:51:37 -0700

    bug fix

--- public/code-files/bookmarklet.js
+++ public/code-files/bookmarklet.js
@@ -1,5 +1,2 @@
 //wsb
-var s=document.createElement("script"); s.src="https://web-scrapping-bookmarklet-7000.herokuapp.com/api/wsb"; document.querySelector("body").appendChild(s);
-
-//sh-bookmarklet
-var s=document.createElement("script"); s.src="https://web-scrapping-bookmarklet-7000.herokuapp.com/api/sh-bookmarklet.json"; document.querySelector("body").appendChild(s);
+var s=document.createElement("script"); s.src="https://bookmarklet-server.herokuapp.com/api/web-scrapping-bookmarklet"; document.querySelector("body").appendChild(s);
