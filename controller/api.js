e308372d593a6d2241e54af4dde15525d66526e6
James Chen <james.lai.chen@gmail.com>
2016-05-04 17:09:51 -0700

    bug fix

--- controller/api.js
+++ controller/api.js
@@ -6,12 +6,13 @@ var path = require('path');
 
 module.exports = function(router){
   router.get("/api/:app_name", function(req,res){
-    var file_name = path.resolve('/public/code-files/'+req.params.app_name);
+    var file_name = path.resolve('public/code-files/'+req.params.app_name+'.js');
+    console.log(file_name);
     models.Mappings.findAll().then(function(mappings){
       res.sendFile(path.resolve(file_name));
     });
   });
-  /* 
+  /*
   router.get("/api/sh-bookmarklet", function(req,res){
     models.Mappings.findAll().then(function(mappings){
       res.sendFile(path.resolve('public/code-files/sh-bookmarklet.js'));
